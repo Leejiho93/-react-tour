88a2146277029d34570a2e864d841dbe671f619d
leejiho <easyho93@gmail.com>
2021-12-27 01:44:16 +0900

    db

---
+++ back/config/config.js
@@ -0,0 +1,27 @@
+require('dotenv').config();
+
+const config  = {
+  development: {
+    username: "root",
+    password: process.env.DB_PASSWORD,
+    database: "test1",
+    host: "127.0.0.1",
+    dialect: "mysql",
+  },
+  test: {
+    username: "root",
+    password: process.env.DB_PASSWORD,
+    database: "test1",
+    host: "127.0.0.1",
+    dialect: "mysql",
+  },
+  production: {
+    username: "root",
+    password: process.env.DB_PASSWORD,
+    database: "test1",
+    host: "127.0.0.1",
+    dialect: "mysql",
+  },
+};
+
+module.exports = config;
