d298f21e249b9eb80699c2d8a6ffdd146917d14c
leejiho <easyho93@gmail.com>
2021-12-27 02:28:29 +0900

    db

--- back/models/sequelize.ts
+++ back/models/sequelize.ts
@@ -1,5 +1,5 @@
 import { Sequelize } from "sequelize";
-import config from "../config";
+import config from "../config/config";
 
 const env =
   (process.env.NODE_ENV as "production" | "test" | "development") ||
