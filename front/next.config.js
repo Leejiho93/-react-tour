efa30a47d598b6b1477a0510267867a3519cf54b
leejiho <easyho93@gmail.com>
2021-12-26 22:12:12 +0900

    gz

--- front/next.config.js
+++ front/next.config.js
@@ -1,12 +1,16 @@
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 })
+const CompressionPlugin = require('compression-webpack-plugin');
 
 module.exports = withBundleAnalyzer({
   compress: true,
   webpack(config, {}){
     const prod = process.env.NODE_ENV === 'production';
     const plugins = [...config.plugins];
+    if (prod) {
+      plugins.push(new CompressionPlugin());
+    }
     return{
         ...config,
         mode: prod ? 'production' : 'development',
