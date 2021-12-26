8342da3c21b4556c76214e696b146185db9e42e8
leejiho <easyho93@gmail.com>
2021-12-27 02:04:18 +0900

    db

--- back/models/sequelize.ts
+++ back/models/sequelize.ts
@@ -5,9 +5,7 @@ const env =
   (process.env.NODE_ENV as "production" | "test" | "development") ||
   "development";
 const { database, username, password } = config[env];
-const sequelize = new Sequelize(database, username, password, {
-  dialect: "mysql",
-});
+const sequelize = new Sequelize(database, username, password, config[env]);
 
 export { sequelize };
 export default sequelize;
