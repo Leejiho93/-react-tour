a9c7462d540dcadd42479e8be1c7c13cd9f62765
leejiho <easyho93@gmail.com>
2021-12-27 23:18:45 +0900

    domain

--- back/index.ts
+++ back/index.ts
@@ -31,8 +31,7 @@ const corsDevOptions = {
   credentials: true,
 };
 const corsProdOptions = {
-  // origin: /wdywg\.site$/,
-  origin: true,
+  origin: /wdywg\.site$/,
   credentials: true,
 };
 passportConfig();
@@ -59,6 +58,7 @@ app.use(
       httpOnly: true,
       secure: false,
       maxAge: 1000 * 60 * 60,
+      domain: prod ? ".wdywg.site" : undefined,
     },
     name: "dqpsa",
   })
