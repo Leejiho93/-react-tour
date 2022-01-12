b0b822f5bd9b66a870fa9a5d982278efb8e1f0b0
leejiho <easyho93@gmail.com>
2022-01-12 10:53:08 +0900

    오픈그래피 적용완료

--- front/src/pages/_app.tsx
+++ front/src/pages/_app.tsx
@@ -44,52 +44,6 @@ const Tour = ({ Component, pageProps }: AppProps) => {
             href="https://fonts.googleapis.com/css2?family=Gowun+Batang&display=swap"
           />
         </Helmet>
-        {/* <Helmet
-          title="어디갈래"
-          htmlAttributes={{ lang: 'ko' }}
-          meta={[
-            { charSet: 'utf-8' },
-            {
-              name: 'viewport',
-              content:
-                'content="width=device-width, initial-scale=0.86, maximum-scale=5.0, minimum-scale=0.86"',
-            },
-            {
-              httpEquiv: 'X-UA-Compatible',
-              content: 'IE=edge',
-            },
-            {
-              name: 'description',
-              content: '대한민국 관광지 소개',
-            },
-            {
-              property: 'og:title',
-              content: '어디갈래',
-            },
-            {
-              property: 'og:description',
-              content: '대한민국 관광지 소개',
-            },
-            {
-              property: 'og:type',
-              content: 'website',
-            },
-            {
-              property: 'og:image',
-              content: 'http://wdywg.site/favicon.ico',
-            },
-          ]}
-          link={[
-            {
-              rel: 'shortcut icon',
-              href: '/favicon.ico',
-            },
-            {
-              rel: 'stylesheet',
-              href: 'https://fonts.googleapis.com/css2?family=Gowun+Batang&display=swap',
-            },
-          ]}
-        /> */}
         <GlobalStyle />
         <Layout>
           <Component {...pageProps} />
--- front/src/pages/_document.tsx
+++ front/src/pages/_document.tsx
@@ -55,23 +55,6 @@ class MyDocument extends Document<Props> {
         <Head>
           {this.props.styles}
           {Object.values(helmet).map((el) => el.toComponent())}
-          {/* <title>어디갈래</title>
-          <meta charSet="UTF-8" />
-          <meta httpEquiv="X-UA-Compatible" />
-          <meta name="description" content="대한민국 관광지 소개" />
-          <meta
-            name="viewport"
-            content='width=device-width, initial-scale=0.86, maximum-scale=5.0, minimum-scale=0.86"'
-          />
-          <meta property="og:title" content="어디갈래" />
-          <meta property="og:description" content="대한민국 관광지 소개" />
-          <meta property="og:type" content="website" />
-          <meta property="og:image" content="http://wdywg.site/favicon.ico" />
-          <link rel="shortcut icon" />
-          <link
-            rel="stylesheet"
-            href="https://fonts.googleapis.com/css2?family=Gowun+Batang&display=swap"
-          /> */}
         </Head>
         <body {...bodyAttrs}>
           <Main />
